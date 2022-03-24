a228e82b1f449bb85da32cfe0b9126eff9b0da8d
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-03-24 06:48:51 +0000

    - Adding and Removing of item stacks - Detection of zero stack items + deletion

--- Inventory/inventoryHandler.py
+++ Inventory/inventoryHandler.py
@@ -1,5 +1,5 @@
 from useful_functions import *
-from Items.item1 import Item1
+from Items.plantItem import PlantItem
 from Items.item2 import Item2
 from Items.item3 import Item3
 
@@ -8,18 +8,39 @@ class InventoryHandler:
 
     def __init__(self, game):
         self.game = game
-        self.inventory = [Item1() for _ in range(133)]
+        self.inventory = [Item2() for _ in range(120)]
 
         self.inventory_size = None
         self.visible_size_full = None
         self.visible_size_limit = None
+        self.inventory.append(PlantItem(4, 0, 9, 3, 5, "crimson"))
+        self.inventory.append(PlantItem(1, 0, 9, 3, 5, "verdant"))
+        self.inventory.append(PlantItem(2, 0, 3, 3, 5, "crystalline"))
+        self.inventory.append(PlantItem(2, 0, 9, 3, 3, "decaying"))
 
         self.update_inventory()
 
     def update_inventory(self):
         self.inventory_size = len(self.inventory)
         self.visible_size_full = clamp(math.ceil(self.inventory_size / 11) * 100, 700, 0)
-        self.visible_size_limit = 100 * (self.inventory_size % 11)
+
+    def add_item(self, item):
+        item_found = False
+        for x in self.inventory:
+            if x.item_ID == item.item_ID:
+                x.stack_size += 1
+                item_found = True
+        if not item_found:
+            self.inventory.append(item)
+        self.update_inventory()
+
+    def remove_item(self, item):
+        item.stack_size -= 1
+        if item.stack_size == 0:
+            self.inventory.remove(item)
+            self.game.menu_handler.inventory_menu.clicked_inv_item = None
+        self.update_inventory()
+
 
 
 
