0cde74947c2f02ab89a9b5f7b4667049922aa318
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-03-26 03:02:56 +0000

    - Descriptions for all primary and secondary plant species

---
+++ Items/plantSpecies.py
@@ -0,0 +1,13 @@
+species_primary = {
+    "Verdant": ["VERDANT :",
+                " This ",
+                " Plant ",
+                " Is",
+                " Happy"]
+}
+species_secondary = {
+    "Verdant": ["VERDANT :",
+                "oooooooooooooooooooooooooooooo"
+
+    ]
+}
