b9fc7ba3627b94563bacea619b75a3e676d33ca0
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-03-26 00:26:11 +0000

    - Split button class - Added text box class, for both multiline and single line text

--- button.py
+++ button.py
@@ -1,44 +1,19 @@
 from useful_functions import *
+from textBox import TextBox
 
 
-class Button:
+class Button(TextBox):
 
     def __init__(self, text, position, size, offset, outline, centered, font_size, colour=GREY):
-        self.rect, self.text_visual = None, None
-        self.x, self.y = position
-        self.offset_x, self.offset_y = offset
-        self.size = size
-        self.font = pygame.font.SysFont("Arial", font_size)
-        self.outline = outline
-        self.centered = centered
-        self.text = text
-        self.surface = pygame.Surface(self.size)
-        self.update_button(text, colour)
+        TextBox.__init__(self, text, position, size, offset, outline, centered, font_size, colour)
+        self.rect = None
 
-    def update_button(self, text, colour=WHITE):
-        self.surface.fill(colour)
-        self.text_visual = self.font.render(text, True, BLACK)
-        if self.centered:
-            self.surface.blit(self.text_visual, (self.size[0]/2 - self.text_visual.get_width()/2,
-                                                 self.size[1]/2 - self.text_visual.get_height()/2))
-        else:
-            self.surface.blit(self.text_visual, (10, 10))
-        if self.outline:
-            pygame.draw.rect(self.surface, BLACK, (0, 0, self.size[0], self.size[1]), 2)
+    def update_button(self, text, colour):
+        self.update_textbox(text, colour)
         self.rect = pygame.Rect(self.x + self.offset_x, self.y + self.offset_y, self.size[0], self.size[1])
 
-    def update_button_multiline(self, text, colour=WHITE):
-        self.surface.fill(colour)
-        for x in range(len(text)):
-            self.text_visual = self.font.render(text[x], True, BLACK)
-            if self.centered:
-                self.surface.blit(self.text_visual, (self.size[0] / 2 - self.text_visual.get_width() / 2,
-                                                     self.size[1] / 2 - self.text_visual.get_height() / 2
-                                                     + self.text_visual.get_height() * x))
-            else:
-                self.surface.blit(self.text_visual, (10, 10 + self.text_visual.get_height() * x))
-            if self.outline:
-                pygame.draw.rect(self.surface, BLACK, (0, 0, self.size[0], self.size[1]), 2)
+    def update_button_multiline(self, text, colour):
+        self.update_textbox_multiline(text, colour)
         self.rect = pygame.Rect(self.x + self.offset_x, self.y + self.offset_y, self.size[0], self.size[1])
 
 
