860eeb9ef054ceedf774e54aab9d9c9c7163cbdb
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-03-26 03:06:36 +0000

    - Added descriptions to each item

--- Items/plantItem.py
+++ Items/plantItem.py
@@ -4,7 +4,7 @@ from Items.baseItem import BaseItem
 
 class PlantItem(BaseItem):
     # stats 0-9
-    def __init__(self, growth, mutation, seed_yield, lifespan, value, plant_type):
+    def __init__(self, growth, mutation, seed_yield, lifespan, value, plant_type_1, plant_type_2):
         BaseItem.__init__(self)
         self.colour = GREEN
         self.stat_growth = growth
@@ -12,16 +12,15 @@ class PlantItem(BaseItem):
         self.stat_yield = seed_yield
         self.stat_lifespan = lifespan
         self.stat_value = value
-        self.plant_type = plant_type
-        self.item_ID = (self.plant_type+str(self.stat_growth)+str(self.stat_mutation)+str(
-            self.stat_yield)+str(self.stat_lifespan)+str(self.stat_value))
+        self.plant_type_1 = plant_type_1
+        self.plant_type_2 = plant_type_2
+        self.item_ID = (self.plant_type_1 + str(self.stat_growth) + str(self.stat_mutation) + str(
+            self.stat_yield) + str(self.stat_lifespan) + str(self.stat_value) + self.plant_type_2)
         self.stack_size = 1
-        self.item_description = ["Seed Type: "+self.plant_type,
-                                 "Growth Rate: "+str(self.stat_growth),
-                                 "Mutation Rate: "+str(self.stat_mutation),
-                                 "Yield: "+str(self.stat_yield),
-                                 "Lifespan: "+str(self.stat_lifespan),
-                                 "Value: "+str(self.stat_value)]
+        self.item_stats_description = ["   Growth Rate: " + str(self.stat_growth),
+                                       " Mutation Rate: " + str(self.stat_mutation),
+                                       "         Yield: " + str(self.stat_yield),
+                                       "      Lifespan: " + str(self.stat_lifespan),
+                                       "         Value: " + str(self.stat_value)]
         # maybe visual hex for stats?
         # visual for item based on stats
-
