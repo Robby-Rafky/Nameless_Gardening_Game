1004be2ae51f151718618ba7e414bcfc8d38a727
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-03-27 22:14:20 +0100

    - Added attributes to be transferred over from items

--- Garden/Plants/basePlant.py
+++ Garden/Plants/basePlant.py
@@ -1,19 +1,40 @@
 from useful_functions import *
-
+from datetime import timedelta
 
 class Plant:
-    def __init__(self):
+    def __init__(self, growth, mutation, seed_yield, lifespan, value, plant_type_1, plant_type_2, description):
         self.plant_age = 0
-        self.adult_age = 1
-        self.decay_age = 1
+        self.adult_age = 5
+        self.decay_age = 20
         self.tick_rate = 1
+        self.stat_growth = growth
+        self.stat_mutation = mutation
+        self.stat_yield = seed_yield
+        self.stat_lifespan = lifespan
+        self.stat_value = value
+        self.plant_type_1 = plant_type_1
+        self.plant_type_2 = plant_type_2
+        self.plant_description = description
+        #test
+        self.colour = L_ORANGE
 
     def tick_plant(self):
         self.plant_age += self.tick_rate
         if self.plant_age >= self.decay_age:
             return True
 
-    def draw_plant(self, surface, pos_x, pos_y, colour=BROWN):
+    def draw_plant(self, surface, pos_x, pos_y):
         size = clamp((self.plant_age / self.adult_age)*100, 100, 0)
-        pygame.draw.rect(surface, colour, (pos_x + 50 - size/2, pos_y + 50 - size/2, size, size))
+        pygame.draw.rect(surface, self.colour, (pos_x + 50 - size/2, pos_y + 50 - size/2, size, size))
 
+    def is_adult(self):
+        if self.plant_age >= self.adult_age:
+            return True
+
+    def get_time_to_adult(self):
+        time = self.adult_age - self.plant_age
+        return str(timedelta(seconds=time))
+
+    def get_time_to_death(self):
+        time = self.decay_age - self.plant_age
+        return str(timedelta(seconds=time))
