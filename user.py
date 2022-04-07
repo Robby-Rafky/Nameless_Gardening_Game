72641bc2489cf9089dd3f5ecf7c07ad77c13f4e3
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-04-07 08:00:07 +0100

    - Actually made the class useful

--- user.py
+++ user.py
@@ -4,4 +4,12 @@ import pygame
 class User:
 
     def __init__(self):
+        self.cash = 10000
 
+    def purchase_check(self, cost):
+        if self.cash - cost < 0:
+            return False
+        else:
+            self.cash -= cost
+            print(self.cash)
+            return True
