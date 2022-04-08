e8428f23cc5080da4508833bc339e26d2d050538
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-04-08 03:42:47 +0100

    - Cleaned up abit

--- Menus/ShopMenu.py
+++ Menus/ShopMenu.py
@@ -1,9 +1,8 @@
-import pygame
-
 from Menus.baseMenu import Menu
-from button import Button
+from UserData.ShopItem import ShopItem
 from textBox import TextBox
 from useful_functions import *
+from Items.plantItem import PlantItem
 
 
 class ShopMenu(Menu):
@@ -11,60 +10,72 @@ class ShopMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game, MENU_GREY)
         self.currency_text = TextBox(" ", (725, 760), (350, 40), (60, 150), True, True, 22, MENU_GREY)
+
         self.garden_shop_text = TextBox("Garden Shop", (10, 10), (860, 40), (70, 160), True, True, 26, LIME_GREEN)
-        self.expand_v_price = 100
-        self.expand_h_price = 100
-        self.expand_button_v = Button("Add row (" + str(self.expand_v_price) + " $)",
-                                      (20, 60), (380, 40), (70, 160), True, True, 20,
-                                      self.afford_colour(self.expand_v_price))
-        self.expand_button_h = Button("Add column (" + str(self.expand_h_price) + " $)",
-                                      (480, 60), (380, 40), (70, 160), True, True, 20,
-                                      self.afford_colour(self.expand_h_price))
+        self.buy_expand_garden_v = ShopItem("Add row", (20, 60), (380, 40), (70, 160), 100)
+        self.buy_expand_garden_h = ShopItem("Add column", (480, 60), (380, 40), (70, 160), 100)
+
+        self.seed_shop_text = TextBox("Seeds Shop", (10, 10), (1760, 40), (70, 370), True, True, 26, LIME_GREEN)
+        self.buy_verdant_seed = ShopItem("Verdant Seed", (10, 170), (260, 40), (70, 370), 0,
+                                         PlantItem(0, 0, 0, 0, 0, "Verdant", "Verdant"))
+
         # find a better way to store price info
         self.surface_garden = pygame.Surface((880, 200))
-
+        self.surface_seed = pygame.Surface((1780, 220))
         self.surface_mutate = pygame.Surface((880, 200))
 
-    def afford_colour(self, price):
-        if self.game.user.cash >= price:
-            return L_ORANGE
-        else:
-            return RED
-
     def shop_menu_events(self):
-        if self.expand_button_v.button_event(self.game.mouse_position) and self.game.garden_handler.garden.size_y < 9:
-            if self.game.user.purchase_check(self.expand_v_price):
+        if self.buy_expand_garden_v.button_event(self.game.mouse_position) and self.game.garden_handler.garden.size_y < 9:
+            if self.game.user.purchase_check(self.buy_expand_garden_v.price):
                 self.game.garden_handler.expand_vertical()
-        if self.expand_button_h.button_event(self.game.mouse_position) and self.game.garden_handler.garden.size_x < 15:
-            if self.game.user.purchase_check(self.expand_h_price):
+                self.buy_expand_garden_h.update_price(self.game.garden_handler.garden.size_y
+                                                      * self.game.garden_handler.garden.size_x * 100)
+                self.buy_expand_garden_v.update_price(self.game.garden_handler.garden.size_y
+                                                      * self.game.garden_handler.garden.size_x * 100)
+        elif self.game.garden_handler.garden.size_y == 9:
+            self.buy_expand_garden_v.update_price()
+        if self.buy_expand_garden_h.button_event(self.game.mouse_position) and self.game.garden_handler.garden.size_x < 15:
+            if self.game.user.purchase_check(self.buy_expand_garden_h.price):
                 self.game.garden_handler.expand_horizontal()
+                self.buy_expand_garden_h.update_price(self.game.garden_handler.garden.size_y
+                                                      * self.game.garden_handler.garden.size_x * 100)
+                self.buy_expand_garden_v.update_price(self.game.garden_handler.garden.size_y
+                                                      * self.game.garden_handler.garden.size_x * 100)
+        elif self.game.garden_handler.garden.size_x == 15:
+            self.buy_expand_garden_h.update_price()
+
+        if self.buy_verdant_seed.button_event(self.game.mouse_position):
+            if self.game.user.purchase_check(self.buy_verdant_seed.price):
+                self.game.inventory_handler.add_item(self.buy_verdant_seed.item)
 
     def draw_garden_shop(self):
         pygame.draw.rect(self.surface_garden, BLACK, (0, 0, 880, 200), 2)
-        self.expand_h_price = self.game.garden_handler.garden.size_y * self.game.garden_handler.garden.size_x * 100
-        self.expand_v_price = self.game.garden_handler.garden.size_y * self.game.garden_handler.garden.size_x * 100
-        if self.game.garden_handler.garden.size_y < 9:
-            self.expand_button_v.update_button("Add row (" + str(self.expand_v_price) + " $)",
-                                               self.afford_colour(self.expand_v_price))
-        else:
-            self.expand_button_v.update_button("Max rows purchased", GREY)
 
-        if self.game.garden_handler.garden.size_x < 15:
-            self.expand_button_h.update_button("Add column (" + str(self.expand_h_price) + " $)",
-                                               self.afford_colour(self.expand_h_price))
-        else:
-            self.expand_button_h.update_button("Max columns purchased", GREY)
+        self.buy_expand_garden_h.update_shop(self.game.user.cash)
+        self.buy_expand_garden_v.update_shop(self.game.user.cash)
+
         self.garden_shop_text.draw_on_surface(self.surface_garden)
-        self.expand_button_v.draw_on_surface(self.surface_garden)
-        self.expand_button_h.draw_on_surface(self.surface_garden)
+        self.buy_expand_garden_v.draw_shop_on_surface(self.surface_garden)
+        self.buy_expand_garden_h.draw_shop_on_surface(self.surface_garden)
+
+    def draw_seed_shop(self):
+        pygame.draw.rect(self.surface_seed, BLACK, (0, 0, 1780, 220), 2)
+
+        self.buy_verdant_seed.update_shop(self.game.user.cash)
+
+        self.seed_shop_text.draw_on_surface(self.surface_seed)
+        self.buy_verdant_seed.draw_shop_on_surface(self.surface_seed)
 
     def draw_shop_items(self):
         self.surface_garden.fill(MENU_GREY)
         self.surface_mutate.fill(MENU_GREY)
+        self.surface_seed.fill(MENU_GREY)
 
         self.draw_garden_shop()
+        self.draw_seed_shop()
 
         self.currency_text.update_textbox("Cash: " + str(self.game.user.cash) + " $", MENU_GREY)
         self.currency_text.draw_on_surface(self.surface)
         self.surface.blit(self.surface_garden, (10, 10))
+        self.surface.blit(self.surface_seed, (10, 220))
         self.surface.blit(self.surface_mutate, (910, 10))
