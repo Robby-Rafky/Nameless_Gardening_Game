6017c23d518191e58b4dd9ae87c1e23c7b8e5fab
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-04-18 03:51:59 +0100

    - Stats for the garden

---
+++ Garden/gardenGlobals.py
@@ -0,0 +1,60 @@
+from useful_functions import *
+
+garden_global = {
+    "stat_magnitude": 100,
+
+    "flat_fertiliser": 0,
+    "mult_fertiliser": 0,
+    "total_fertiliser": 0,
+
+    "total_adult": 1,
+    "total_death": 1,
+    "total_rate": 1,
+    "total_mutation": 1,
+    "total_yield": 1,
+    "total_value": 1,
+    "total_ability": 1,
+
+    "skill_tree_adult": 1,
+    "skill_tree_death": 1,
+    "skill_tree_rate": 1,
+    "skill_tree_mutation": 1,
+    "skill_tree_yield": 1,
+    "skill_tree_value": 1,
+    "skill_tree_ability": 1,
+
+    "garden_global_adult": 1,
+    "garden_global_death": 1,
+    "garden_global_rate": 1,
+    "garden_global_mutation": 1,
+    "garden_global_yield": 1,
+    "garden_global_value": 1,
+    "garden_global_ability": 1
+}
+
+
+def add_stat(stat, value):
+    garden_global[stat] += value
+    update_totals(stat.rsplit("_", 1)[1])
+
+
+def subtract_stat(stat, value):
+    garden_global[stat] -= value
+    update_totals(stat.rsplit("_", 1)[1])
+
+
+def multiply_stat(stat, value):
+    garden_global[stat] = garden_global[stat] * value
+    update_totals(stat.rsplit("_", 1)[1])
+
+
+def divide_stat(stat, value):
+    garden_global[stat] = garden_global[stat] / value
+    update_totals(stat.rsplit("_", 1)[1])
+
+
+def update_totals(stat):
+    if stat is not "fertiliser":
+        garden_global["total_"+stat] = garden_global["skill_tree_"+stat] * garden_global["garden_global_"+stat]
+    else:
+        garden_global["total_" + stat] = garden_global["flat_" + stat] * garden_global["mult_" + stat]
