16cdabde41c2bdf4f8387503f53cd4b37d47e40c
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-04-24 02:29:12 +0100

    - Beginning of skill tree

--- Menus/SkillsMenu.py
+++ Menus/SkillsMenu.py
@@ -1,13 +1,16 @@
 from Menus.baseMenu import Menu
 from button import Button
 from useful_functions import *
+from SkillTree.skillTreeData import skill_tree_data
 
 
 class SkillTreeMenu(Menu):
 
     def __init__(self, game):
         Menu.__init__(self, game, MENU_GREY)
-
+        self.all_passives = []
+        for item in skill_tree_data:
+            self.all_passives.append(Button)
 
     def skill_tree_menu_events(self):
         pass
