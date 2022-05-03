4d0f44faa32fce43c0025c5cbecc04c77375d507
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-05-03 03:28:46 +0100

    - Zoom now targets the location being looked at (rather than the origin)

--- Menus/SkillsMenu.py
+++ Menus/SkillsMenu.py
@@ -24,7 +24,7 @@ class SkillTreeMenu(Menu):
         self.skill_surface = pygame.Surface((1720, 680))
         self.action_surface = pygame.Surface((1720, 40))
         self.zoom_scale = 1
-        self.scroll_x = 0
+        self.scroll_x = 10000
         self.scroll_y = 0
         for item in self.passive_data:
             a = self.passive_data[item]
@@ -38,34 +38,32 @@ class SkillTreeMenu(Menu):
     def draw_skill_tree(self):
         self.skill_surface.fill(MENU_GREY)
         self.action_surface.fill(MENU_GREY)
-        scaled_scroll_x = self.scroll_x * self.zoom_scale
-        scaled_scroll_y = self.scroll_y * self.zoom_scale
+        looking_at = [860 - self.scroll_x, 340 - self.scroll_y]
 
-        pygame.draw.rect(self.skill_surface, BLACK, (860, 340, 10, 10), 0)
-        pygame.draw.rect(self.skill_surface, L_BLUE, (self.scroll_x, self.scroll_y, 10, 10), 0)
         for item in self.passive_data:
             for connection in self.passive_data[item]["connections"]:
-                pos_x = self.passive_data[connection]["x"] * self.zoom_scale + 860
-                pos_y = self.passive_data[connection]["y"] * self.zoom_scale + 340
-                pos_x2 = self.passive_data[item]["x"] * self.zoom_scale + 860
-                pos_y2 = self.passive_data[item]["y"] * self.zoom_scale + 340
+                pos_x = (self.scroll_x - self.passive_data[connection]["x"]) * self.zoom_scale + self.scroll_x
+                pos_y = (self.scroll_y - self.passive_data[connection]["y"]) * self.zoom_scale + self.scroll_y
+                pos_x2 = (self.scroll_x - self.passive_data[item]["x"]) * self.zoom_scale + self.scroll_x
+                pos_y2 = (self.scroll_y - self.passive_data[item]["y"]) * self.zoom_scale + self.scroll_y
                 if self.passive_data[connection]["allocated"] and self.passive_data[item]["allocated"]:
                     colour = GREEN
                 else:
                     colour = GREY
-                pygame.draw.line(self.skill_surface, colour, (pos_x + self.scroll_x,
-                                                              pos_y + self.scroll_y),
-                                 (pos_x2 + self.scroll_x,
-                                  pos_y2 + self.scroll_y),
+                pygame.draw.line(self.skill_surface, colour, (pos_x + looking_at[0],
+                                                              pos_y + looking_at[1]),
+                                 (pos_x2 + looking_at[0],
+                                  pos_y2 + looking_at[1]),
                                  int(20 * self.zoom_scale))
 
         for item in self.all_passives:
             item.change_colour(self.passive_data[item.passive_id]["allocated"],
                                item.mouse_over(self.game.mouse_pos), self.zoom_scale * 100)
-            item.scaled_x = item.x * self.zoom_scale
-            item.scaled_y = item.y * self.zoom_scale
-            print(self.scroll_x, self.scroll_y)
-            item.draw_passive(self.skill_surface, self.scroll_x, self.scroll_y)
+
+            item.scaled_x = (self.scroll_x - item.x) * self.zoom_scale + self.scroll_x
+            item.scaled_y = (self.scroll_y - item.y) * self.zoom_scale + self.scroll_y
+
+            item.draw_passive(self.skill_surface, looking_at[0], looking_at[1])
 
         pygame.draw.rect(self.skill_surface, BLACK, (0, 0, 1720, 680), 2)
         pygame.draw.rect(self.action_surface, BLACK, (0, 0, 1720, 40), 2)
