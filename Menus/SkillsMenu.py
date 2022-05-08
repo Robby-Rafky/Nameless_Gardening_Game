1781a76bc4b07a167153541fbdc5aed419ec8de2
Im-a-Degen <58045054+Im-a-Degen@users.noreply.github.com>
2022-05-08 06:45:04 +0100

    - Updated to have connections list all connections, not just prerequisite connections - Added checking for connections and requirements for allocating passives

--- Menus/SkillsMenu.py
+++ Menus/SkillsMenu.py
@@ -13,6 +13,8 @@ from useful_functions import *
 # Small passives: Stats + name, No description
 # Large passives: stats + name + desc
 # The item string is the ID for the passive, but the passive also has its own name
+# Search connections for stuff to be required before allocation (using OR)
+# Requirements are optional, and dont have to be connected (using AND)
 
 
 class SkillTreeMenu(Menu):
@@ -23,17 +25,29 @@ class SkillTreeMenu(Menu):
         self.all_passives = []
         self.skill_surface = pygame.Surface((1720, 680))
         self.action_surface = pygame.Surface((1720, 40))
-        self.zoom_scale = 1
-        self.scroll_x = 10000
-        self.scroll_y = 0
+        self.zoom_scale = 0.2
+        self.scroll_x = 9000
+        self.scroll_y = 400
         for item in self.passive_data:
             a = self.passive_data[item]
-            self.all_passives.append(Passive(a["type"], a["x"], a["y"], item, a["tier"]))
+            self.all_passives.append(Passive(a["type"], a["x"], a["y"], item, a["tier"], a["group"]))
 
     def skill_tree_menu_events(self):
         for item in self.all_passives:
             if item.passive_events(self.game.mouse_pos):
-                self.passive_data[item.passive_id]["allocated"] = True
+                allocatable = False
+                for connection in self.passive_data[item.passive_id]["connections"]:
+                    if self.passive_data[connection]["allocated"]:
+                        allocatable = True
+                if not self.passive_data[item.passive_id]["required"]:
+                    requirements_met = False
+                else:
+                    requirements_met = True
+                for requirement in self.passive_data[item.passive_id]["required"]:
+                    if not self.passive_data[requirement]["allocated"]:
+                        requirements_met = False
+                if allocatable or requirements_met:
+                    self.passive_data[item.passive_id]["allocated"] = True
 
     def draw_skill_tree(self):
         self.skill_surface.fill(MENU_GREY)
@@ -58,7 +72,7 @@ class SkillTreeMenu(Menu):
 
         for item in self.all_passives:
             item.change_colour(self.passive_data[item.passive_id]["allocated"],
-                               item.mouse_over(self.game.mouse_pos), self.zoom_scale * 100)
+                               item.mouse_over(self.game.mouse_pos), self.zoom_scale)
 
             item.scaled_x = (self.scroll_x - item.x) * self.zoom_scale + self.scroll_x
             item.scaled_y = (self.scroll_y - item.y) * self.zoom_scale + self.scroll_y
